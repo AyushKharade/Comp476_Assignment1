91c8807b80833c9ec009630f274be308cb778fc2
AyushKharade <ayush.kharade999@gmail.com>
2020-01-26 17:25:15 -0500

    weird camera script

---
+++ Comp476_Assignment1/Assets/Scripts/Misc/Camera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform Target;
+    [Range(25,50)]
+    public float zoomOffset;
+    public float xOffset=30;
+    public float yOffset=30;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null)
+        {
+            Vector3 pos = Target.position;
+            pos.y += zoomOffset;
+            pos.x += xOffset;
+            pos.z += yOffset;
+
+            transform.position = pos;
+            transform.LookAt(Target.position);
+
+        }
+    }
+}
